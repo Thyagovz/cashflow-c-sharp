202e967d475b83b6b299e3924af498973352bb36
Thyago Euclides <flooddk@gmail.com>
2025-11-20 10:05:58 -0400

    Standardize API error responses
    Adding:
    - Introduces a standardized structure for API error messages. (src/CashFlow.Communication/Responses/ResponseErrorJson.cs)
    
    Updating:
    - Uses `ResponseErrorJson` to return consistent error payloads for API exceptions. (src/CashFlow.Api/Controllers/ExpensesController.cs)

--- CashFlow/src/CashFlow.Api/Controllers/ExpensesController.cs
+++ CashFlow/src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers;
@@ -20,14 +21,18 @@ public class ExpensesController : ControllerBase
 
             return Created(string.Empty, response);
         }
-        catch (ArgumentException ex) 
+        catch (ArgumentException ex)
         {
-            return BadRequest(ex.Message);
+            var errorResponse = new ResponseErrorJson(ex.Message);
+
+            return BadRequest(errorResponse);
         }
-        catch 
+        catch
         {
-            return StatusCode(StatusCodes.Status500InternalServerError,"unknown error");
+            var errorResponse = new ResponseErrorJson("unknown error");
+
+            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
         }
-  
+
     }
 }
