ff27b1b900592f3c9c4f783b58e03704dbf07c97
Thyago Euclides <flooddk@gmail.com>
2025-12-11 11:03:10 -0400

    Implement custom font resolver for PDF reports
    Adding:
    - Implement custom font resolution logic.
    - Define font constant names.
    
    Updating:
    - Integrate custom font resolver into PDF generation.
    
    Configuration:
    - Add PDFsharp-MigraDoc package reference.

---
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/ExpensesReportFontResolver.cs
@@ -0,0 +1,18 @@
+﻿using PdfSharp.Charting;
+using PdfSharp.Fonts;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+
+public class ExpensesReportFontResolver : IFontResolver
+{
+    byte[]? IFontResolver.GetFont(string faceName)
+    {
+        throw new NotImplementedException();
+    }
+
+    FontResolverInfo? IFontResolver.ResolveTypeface(string familyName, bool bold, bool italic)
+    {
+
+        return new FontResolverInfo(familyName);
+    }
+}
