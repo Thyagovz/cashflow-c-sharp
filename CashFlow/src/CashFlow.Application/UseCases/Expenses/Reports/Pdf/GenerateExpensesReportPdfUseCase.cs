69e0210cb41c852ba614822adff1b6dd4e1f925e
Thyago Euclides <flooddk@gmail.com>
2025-12-13 10:54:56 -0400

    Commit Title: Add header elements to expense report PDF
    Updating:
    - Implement table, image, and text for PDF report header.

--- CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -30,6 +30,23 @@ public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCas
         var document = CreateDocument(month);
         var page = CreatePage(document);
 
+        var table = page.AddTable();
+        table.AddColumn();
+        table.AddColumn();
+
+        var row = table.AddRow();
+        row.Cells[0].AddImage("C:\\Users\\flood\\OneDrive\\Imagens\\Pessoais\\img.jpg");
+
+        row.Cells[1].AddParagraph("Hey, Thyago Euclides");
+        row.Cells[1].Format.Font = new Font { Name = FontHelper.RALEWAY_BLACK, Size = 16 };
+
+
+
+
+
+
+
+
         var paragraph = page.AddParagraph();
         var title = string.Format(ResourceReportGenerationMessages.TOTAL_SPENT_IN, month.ToString("Y"));
 
