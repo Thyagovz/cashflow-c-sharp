070eab7d0571f87807ebc5530431c7bbfe73be7a
Thyago Euclides <flooddk@gmail.com>
2026-01-16 05:20:28 -0400

    Add InMemoryTest flag to skip DB setup in test environments
    Introduce InMemoryTest config flag and IsTestEnvironment extension to distinguish test environments. Prevent database migrations and DbContext registration when running tests, enabling use of in-memory or mock databases. This avoids real database usage during automated testing.

--- CashFlow/src/CashFlow.Api/Program.cs
+++ CashFlow/src/CashFlow.Api/Program.cs
@@ -2,6 +2,7 @@ using CashFlow.Api.Filters;
 using CashFlow.Api.Middleware;
 using CashFlow.Application;
 using CashFlow.Infrastructure;
+using CashFlow.Infrastructure.Extensions;
 using CashFlow.Infrastructure.Migrations;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
@@ -85,7 +86,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-await MigrateDataBase();
+if (builder.Configuration.IsTestEnvironment() == false)
+{
+    await MigrateDataBase();
+}
 
 app.Run();
 
