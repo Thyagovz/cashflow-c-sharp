94631a0cf7bda9d1311c66f15a1e891f329aaf22
Thyago Euclides <flooddk@gmail.com>
2026-01-16 11:03:28 -0400

    Enforce user scoping for expense registration and updates
    Register and update expense use cases now require the logged-in user and associate expenses with the user's ID. Repository methods and implementations have been updated to ensure users can only modify their own expenses, improving security and data integrity. Minor formatting adjustments included.

--- CashFlow/src/CashFlow.Infrastructure/DataAccess/Repositories/ExpenseRepository.cs
+++ CashFlow/src/CashFlow.Infrastructure/DataAccess/Repositories/ExpenseRepository.cs
@@ -25,9 +25,9 @@ internal class ExpensesRepository : IExpensesReadOnlyRepository, IExpensesWriteO
     {
         return await _dbContext.Expenses.AsNoTracking().FirstOrDefaultAsync(e => e.Id == id);
     }
-    async Task<Expense?> IExpensesUpdateOnlyRepository.GetById(long id)
+    async Task<Expense?> IExpensesUpdateOnlyRepository.GetById(User user, long id)
     {
-        return await _dbContext.Expenses.FirstOrDefaultAsync(e => e.Id == id);
+        return await _dbContext.Expenses.FirstOrDefaultAsync(e => e.Id == id && e.UserId == user.Id);
     }
     public async Task<bool> Delete(long id)
     {
@@ -44,14 +44,14 @@ internal class ExpensesRepository : IExpensesReadOnlyRepository, IExpensesWriteO
 
     public void Update(Expense expense)
     {
-       _dbContext.Expenses.Update(expense);
+        _dbContext.Expenses.Update(expense);
     }
 
     public async Task<List<Expense>> FilterByMonth(DateOnly date)
     {
         var startDate = new DateTime(year: date.Year, month: date.Month, day: 1).Date;
         var daysInMonth = DateTime.DaysInMonth(year: date.Year, month: date.Month);
-        var endDate = new DateTime(year: date.Year, month: date.Month, day: daysInMonth, hour:23, minute: 59, second: 59);
+        var endDate = new DateTime(year: date.Year, month: date.Month, day: daysInMonth, hour: 23, minute: 59, second: 59);
 
         return await _dbContext
             .Expenses
